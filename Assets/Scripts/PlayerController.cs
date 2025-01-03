6ede544f52f74556da478b05f228ea05f77de0d5
Siinestesiia <emir-cykman@hotmail.com>
2025-01-03 19:35:29 -0300

    Player Position and Weapon Activation.
    I Handle the Player Position by taking the mouse position and convert it from Screen Point Position to World Space Position. I added a clamp so the Spaceship won't go beyond the boundaries of the game window.
    I wrote a simple method for checking if the two mouse buttons are being held independently.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,43 +5,72 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Player Input
     PlayerInput playerInput;
-
-    // Input actions
+    
     InputAction moveAction;
-    private InputAction leftAttackAction;
-    private InputAction rightAttackAction;
+    InputAction leftWeaponAttack;
+    InputAction rightWeaponAttack;
 
-    // Movement variable stores x and y values.
-    Vector2 mousePosition;
+
+    private Vector2 mousePosition;
+    [Header ("Movement Boundaries of the Screen.")]
+    [SerializeField] float minX = -28;
+    [SerializeField] float maxX = 28;
+    [SerializeField] float minZ = -15;
+    [SerializeField] float maxZ = 14;
+
+    private float cameraHeight;
 
     
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
-
-        // The name in "" must match the action name.
         moveAction = playerInput.actions["move"];
-        // leftAttackAction = playerInput.actions["LeftWeaponAttack"];       
-        // rightAttackAction = playerInput.actions["RightWeaponAttack"];       
+        leftWeaponAttack = playerInput.actions["LeftWeaponAttack"];
+        rightWeaponAttack = playerInput.actions["RightWeaponAttack"];
+
+        cameraHeight = Camera.main.transform.position.y;
     }
 
     void Update()
     {
-        float cameraHeight = Camera.main.transform.position.y;
-    
-        // Handling Mouse Movement
+        SetPlayerPosition();
+        WeaponAttack();
+    }
+
+
+    // Spaceship Movement ----------------------------------------------------
+    private void SetPlayerPosition()
+    {
         mousePosition = moveAction.ReadValue<Vector2>();
+        
         Vector3 screenPoint = new Vector3(mousePosition.x, mousePosition.y, cameraHeight);
         Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(screenPoint);
 
         Vector3 newPosition = new Vector3(worldMousePosition.x, transform.position.y, worldMousePosition.z);
+        transform.position = ClampPosition(newPosition);
+    }
 
-        Debug.Log($"World Mouse Position: {worldMousePosition} | Transformed Position: {newPosition}");
-        transform.position = newPosition;
+    // Set the screen boundaries for the Spaceship movement.
+    private Vector3 ClampPosition(Vector3 positionCoordinates)
+    {
+        float clampedX = Mathf.Clamp(positionCoordinates.x, minX, maxX);
+        float clampedZ = Mathf.Clamp(positionCoordinates.z, minZ, maxZ);
+        
+        return new Vector3(clampedX, positionCoordinates.y, clampedZ);
+    }
 
-        // Debug.Log($"Left Attack Triggered: {leftAttackAction.triggered}");
-        // Debug.Log($"Right Attack Triggered: {rightAttackAction.triggered}");
+    // Spaceship Weapons ------------------------------------------------------
+    private void WeaponAttack()
+    {
+        if (leftWeaponAttack.IsPressed())
+        {
+            Debug.Log("Shooting left weapon!");
+        }
+        
+        if (rightWeaponAttack.IsPressed())
+        {
+            Debug.Log("Shooting right weapon!");
+        }
     }
 }
